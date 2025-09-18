a1f6829e70d316ea51d61902dbb18eb7ce85d5ad
Ahmed Al-guniad <Ahmed-Ali123-coder>
2025-09-18 15:42:02 +0300

    Writing in the file

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ print("Welcome to git")
 
 try : 
     with open("welcome.txt" , "r+") as file : 
-        print(file.read)
+        file.write("Hello from python and git\n")
+        file.seek(0)
+        for line in file : 
+            print(line.strip())
 except FileNotFoundError as ex :
     print("File not found error")
