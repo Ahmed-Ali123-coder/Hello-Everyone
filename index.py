d70dde35d7eedd1d4fa67aa3e8c32e7fafeef23d
Ahmed Al-guniad <Ahmed-Ali123-coder>
2025-09-18 15:34:09 +0300

    Reading from file

--- index.py
+++ index.py
@@ -1,2 +1,8 @@
 print("Hello form index file")
 print("Welcome to git")
+
+try : 
+    with open("welcome.txt" , "r+") as file : 
+        print(file.read)
+except FileNotFoundError as ex :
+    print("File not found error")
