aebe3d08581037afb97c548b5d9f6002f7493da7
Ahmed Al-guniad <Ahmed-Ali123-coder>
2025-09-19 16:24:36 +0300

    dfkj

--- index.py
+++ index.py
@@ -1,15 +1,11 @@
 print("Hello form index file")
 print("Welcome to git")
 
-try : 
-<<<<<<< HEAD
-    with open("welcome.txt" , "+r") as file : 
-=======
-    with open("welcome.txt" , "+a") as file : 
+try:
+    with open("welcome.txt", "a+") as file:
         file.write("Writing someting in a file\n")
         file.seek(0)
->>>>>>> master
-        for line in file : 
+        for line in file:
             print(line.strip())
-except FileNotFoundError as ex :
+except FileNotFoundError as ex:
     print("File not found error")
